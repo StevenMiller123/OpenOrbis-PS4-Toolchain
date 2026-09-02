78e04628a8beea80053928694824fbfad30d5b85
Stephen Miller <56742918+StevenMiller123@users.noreply.github.com>
2026-09-01 20:00:37 -0500

    NpAppInfoInt, NpAppLaunchLinkInt, NpAppLaunchLink2Int
    Again, sourced from extracted VSH 9.00 IL.

--- include/orbis/NpUtility.h
+++ include/orbis/NpUtility.h
@@ -2,124 +2,77 @@
 #define _SCE_NP_UTIL_H_
 
 #include <stdint.h>
+#include <orbis/_types/Np.h>
 
 #ifdef __cplusplus 
 extern "C" {
 #endif
 
-// Empty Comment
-void sceNpAppInfoIntAbortRequest();
-// Empty Comment
-void sceNpAppInfoIntCheckAvailability();
-// Empty Comment
-void sceNpAppInfoIntCheckAvailabilityAll();
-// Empty Comment
-void sceNpAppInfoIntCheckServiceAvailability();
-// Empty Comment
-void sceNpAppInfoIntCheckServiceAvailabilityAll();
-// Empty Comment
-void sceNpAppInfoIntCreateRequest();
-// Empty Comment
-void sceNpAppInfoIntDestroyRequest();
-// Empty Comment
-void sceNpAppLaunchLinkIntAbortRequest();
-// Empty Comment
-void sceNpAppLaunchLinkIntCreateRequest();
-// Empty Comment
-void sceNpAppLaunchLinkIntDestroyRequest();
-// Empty Comment
-void sceNpAppLaunchLinkIntGetCompatibleTitleIdList();
-// Empty Comment
-void sceNpAppLaunchLinkIntGetCompatibleTitleIdNum();
-// Empty Comment
+int32_t sceNpAppInfoIntAbortRequest(int32_t req_id);
+int32_t sceNpAppInfoIntCheckAvailability(int32_t req_id, OrbisNpOnlineId* online_id, int32_t feature, OrbisNpTitleId* title_id, bool* is_available);
+int32_t sceNpAppInfoIntCheckAvailabilityA(int32_t req_id, int32_t user_id, int32_t feature, OrbisNpTitleId* title_id, bool* is_available);
+int32_t sceNpAppInfoIntCheckAvailabilityAll(int32_t req_id, OrbisNpOnlineId* online_id, int32_t feature, OrbisNpTitleId title_ids[], uint64_t title_id_num, bool* is_available);
+int32_t sceNpAppInfoIntCheckAvailabilityAllA(int32_t req_id, int32_t user_id, int32_t feature, OrbisNpTitleId title_ids[], uint64_t title_id_num, bool* is_available);
+int32_t sceNpAppInfoIntCheckServiceAvailability(int32_t req_id, OrbisNpOnlineId* online_id, int32_t feature, int32_t service_provider, OrbisNpTitleId* title_id, bool* is_available);
+int32_t sceNpAppInfoIntCheckServiceAvailabilityA(int32_t req_id, int32_t user_id, int32_t feature, int32_t service_provider, OrbisNpTitleId* title_id, bool* is_available);
+int32_t sceNpAppInfoIntCheckServiceAvailabilityAll(int32_t req_id, OrbisNpOnlineId* online_id, int32_t feature, int32_t service_provider, OrbisNpTitleId title_ids[], uint64_t title_id_num, bool* is_available);
+int32_t sceNpAppInfoIntCheckServiceAvailabilityAllA(int32_t req_id, int32_t user_id, int32_t feature, int32_t service_provider, OrbisNpTitleId title_ids[], uint64_t title_id_num, bool* is_available);
+int32_t sceNpAppInfoIntCreateRequest(int32_t* req_id);
+int32_t sceNpAppInfoIntDestroyRequest(int32_t req_id);
+int32_t sceNpAppLaunchLinkIntAbortRequest(int32_t req_id);
+int32_t sceNpAppLaunchLinkIntCreateRequest(int32_t user_id, OrbisNpTitleId* title_id, int32_t* req_id);
+int32_t sceNpAppLaunchLinkIntDestroyRequest(int32_t req_id);
+int32_t sceNpAppLaunchLinkIntGetCompatibleTitleIdList(int32_t req_id, OrbisNpTitleId title_ids[], uint64_t id_num);
+int32_t sceNpAppLaunchLinkIntGetCompatibleTitleIdNum(int32_t req_id, uint64_t* id_num);
+int32_t sceNpAppLaunchLink2IntAbortRequest(int32_t req_id);
+int32_t sceNpAppLaunchLink2IntCreateRequest(int32_t user_id, OrbisNpTitleId* title_id, int32_t* req_id);
+int32_t sceNpAppLaunchLink2IntDestroyRequest(int32_t req_id);
+int32_t sceNpAppLaunchLink2IntGetCompatibleTitleIdList(int32_t req_id, OrbisNpTitleId title_ids[], uint64_t id_num);
+int32_t sceNpAppLaunchLink2IntGetCompatibleTitleIdNum(int32_t req_id, uint64_t* id_num);
 void sceNpBandwidthTestAbort();
-// Empty Comment
 void sceNpBandwidthTestGetStatus();
-// Empty Comment
 void sceNpBandwidthTestInitStart();
-// Empty Comment
 void sceNpBandwidthTestShutdown();
-// Empty Comment
 void sceNpBandwidthTestShutdownInt();
-// Empty Comment
 void sceNpLookupAbortRequest();
-// Empty Comment
 void sceNpLookupCreateAsyncRequest();
-// Empty Comment
 void sceNpLookupCreateRequest();
-// Empty Comment
 void sceNpLookupCreateTitleCtx();
-// Empty Comment
 void sceNpLookupCreateTitleCtxA();
-// Empty Comment
 void sceNpLookupDeleteRequest();
-// Empty Comment
 void sceNpLookupDeleteTitleCtx();
-// Empty Comment
 void sceNpLookupNpId();
-// Empty Comment
 void sceNpLookupPollAsync();
-// Empty Comment
 void sceNpLookupSetTimeout();
-// Empty Comment
 void sceNpLookupWaitAsync();
-// Empty Comment
 void sceNpServiceChecker2IntAbortRequest();
-// Empty Comment
 void sceNpServiceChecker2IntCreateRequest();
-// Empty Comment
 void sceNpServiceChecker2IntDestroyRequest();
-// Empty Comment
 void sceNpServiceChecker2IntGetServiceAvailability();
-// Empty Comment
 void sceNpServiceChecker2IntIsSetServiceType();
-// Empty Comment
 void sceNpServiceCheckerIntAbortRequest();
-// Empty Comment
 void sceNpServiceCheckerIntCreateRequest();
-// Empty Comment
 void sceNpServiceCheckerIntDestroyRequest();
-// Empty Comment
 void sceNpServiceCheckerIntGetAvailability();
-// Empty Comment
 void sceNpServiceCheckerIntGetAvailabilityList();
-// Empty Comment
 void sceNpServiceCheckerIntIsCached();
-// Empty Comment
 void sceNpTitleMetadataIntAbortRequest();
-// Empty Comment
 void sceNpTitleMetadataIntCreateRequest();
-// Empty Comment
 void sceNpTitleMetadataIntDeleteRequest();
-// Empty Comment
 void sceNpTitleMetadataIntGetInfo();
-// Empty Comment
 void sceNpUtilityInit();
-// Empty Comment
 void sceNpUtilityTerm();
-// Empty Comment
 void sceNpWordFilterAbortRequest();
-// Empty Comment
 void sceNpWordFilterCensorComment();
-// Empty Comment
 void sceNpWordFilterCreateAsyncRequest();
-// Empty Comment
 void sceNpWordFilterCreateRequest();
-// Empty Comment
 void sceNpWordFilterCreateTitleCtx();
-// Empty Comment
 void sceNpWordFilterCreateTitleCtxA();
-// Empty Comment
 void sceNpWordFilterDeleteRequest();
-// Empty Comment
 void sceNpWordFilterDeleteTitleCtx();
-// Empty Comment
 void sceNpWordFilterPollAsync();
-// Empty Comment
 void sceNpWordFilterSanitizeComment();
-// Empty Comment
 void sceNpWordFilterSetTimeout();
-// Empty Comment
 void sceNpWordFilterWaitAsync();
 
 #ifdef __cplusplus
