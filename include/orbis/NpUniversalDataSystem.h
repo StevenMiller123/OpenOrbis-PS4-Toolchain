c884c2c9b8b049792bceed32746dbf02603e8b72
Stephen Miller <56742918+StevenMiller123@users.noreply.github.com>
2026-09-01 21:27:16 -0500

    sceNpUniversalDataSystem

---
+++ include/orbis/NpUniversalDataSystem.h
@@ -0,0 +1,16 @@
+#ifndef _SCE_NP_UDS_H_
+#define _SCE_NP_UDS_H_
+
+#include <stdint.h>
+#include <orbis/_types/Np.h>
+
+#ifdef __cplusplus 
+extern "C" {
+#endif
+
+int32_t sceNpUniversalDataSystemIntDeleteAllRecord();
+
+#ifdef __cplusplus
+}
+#endif
+#endif
